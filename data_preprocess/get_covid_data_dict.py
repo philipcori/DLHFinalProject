37619c088b046b97fb1ac357ae8f015edee9bab1
samuely4 <61636645+samuely4@users.noreply.github.com>
2021-04-08 22:29:45 -0400

    Update get_covid_data_dict.py
    get_covid_data_dict changes

--- data_preprocess/get_covid_data_dict.py
+++ data_preprocess/get_covid_data_dict.py
@@ -2,17 +2,17 @@ import csv
 import os
 import pickle
 
-data_root_dir = './covid-chestxray-dataset-master'
-image_root_dir = './covid-chestxray-dataset-master/images'
+data_root_dir = './original data/covid-chestxray-dataset-master'
+image_root_dir = './original data/covid-chestxray-dataset-master/images'
 info_file_name = 'metadata.csv'
 
 info_path = os.path.join(data_root_dir, info_file_name)
 
 data_dict = {}
 
-covid = ['COVID-19']
-pneumonia_virus = ['SARS']
-pneumonia_bacteria = ['Pneumocystis','Streptococcus']
+covid = ['Pneumonia/Viral/COVID-19']
+pneumonia_virus = ['Pneumonia/Viral/SARS']
+pneumonia_bacteria = ['Pneumonia/Fungal/Pneumocystis','Pneumonia/Bacterial/Streptococcus']
 normal = ['No Finding']
 x0 = 0
 x1 = 0
@@ -20,7 +20,7 @@ x2 = 0
 x3 = 0
 n_ct = 0
 n_ards = 0
-with open(info_path,'r') as f:
+with open(info_path,'r', encoding="UTF-8") as f:
   csv_reader = csv.reader(f)
   i = 0 
   for row in csv_reader:
@@ -29,10 +29,18 @@ with open(info_path,'r') as f:
       continue
     patient_id = row[0]
     subject_id = row[1]
-    view = row[6]
-    image_name = row[10]
+    view = row[18]
+    image_name = row[23]
     disease = row[4]
-    modality = row[7]
+    modality = row[19]
+    
+    print("patient id is: " + str(patient_id))
+    print("subject id is: " + str(subject_id))
+    print("view is: " + str(view))
+    print("image_name is: " + str(image_name))
+    print("disease is: " + str(disease))
+    print("modality is: " + str(modality))
+    print('--------------------------------------------------------')
     if 'ray' not in modality:
       n_ct += 1
       continue
@@ -122,11 +130,13 @@ print (v0, v1, v2, v3)
 print (w0, w1, w2, w3)
 #pickle.dump(data_dict, open('./data_preprocess/formal_covid_dict_ap.pkl','wb'))
 ##pickle.dump(pa_list, open('pa_list.pkl','wb'))
-###saved_path = './data_preprocess/formal_covid_dict.pkl'
-###if os.path.exists(saved_path):
-###  os.remove(saved_path)
-###pickle.dump(data_dict, open(saved_path,'wb'))
-###print ('finish')
+saved_path = os.path.join(os.getcwd(), "data_preprocess", "formal_covid_dict_ap.pkl")
+print(saved_path)
+#'./data_preprocess/formal_covid_dict.pkl'
+if os.path.exists(saved_path):
+  os.remove(saved_path)
+pickle.dump(data_dict, open(saved_path,'wb'))
+print ('finish')
 ##
 ###             Xray	
 ###新冠	          111	 
